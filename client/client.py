66d114f8ef0b26eae17b59d71e912cf3ac56b777
Maizlow <johan@maizlow.se>
2023-07-25 11:18:25 +0200

    Enable publishing to AWS MQTT

--- client/client.py
+++ client/client.py
@@ -132,7 +132,7 @@ async def main():
                             "value": unpublished["value"],
                             "timestamp": unpublished["timestamp"]
                         }
-                        #await aws_mqtt.publish_to_topic(con, TOPIC_DATA, dumps(payload), mqtt.QoS.AT_LEAST_ONCE)
+                        await aws_mqtt.publish_to_topic(con, TOPIC_DATA, dumps(payload), mqtt.QoS.AT_LEAST_ONCE)
                         publishedIds.append(unpublished["_id"])
 
                     db.deleteDataPoints(publishedIds)
