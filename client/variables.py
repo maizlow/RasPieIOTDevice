ec622a2256d46d505bf0783d76f5c5fd6aef5e5b
Maizlow <johan@maizlow.se>
2023-07-25 08:35:18 +0200

    Updated relative path of certificates

--- client/variables.py
+++ client/variables.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 cwd = Path.cwd()
-rel_path1 = "../../"
+rel_path1 = "../"
 
 CERT_FILEPATH = (cwd / rel_path1 / "IOT_Demo_Certs/device.pem.crt").resolve().__str__() #"/home/abece/certs/device.pem.crt"
 PRI_KEY_FILEPATH = (cwd / rel_path1 / "IOT_Demo_Certs/private.pem.key").resolve().__str__() #"/home/abece/certs/private.pem.key"
