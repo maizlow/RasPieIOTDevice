37c469c20538fc91cccaace74d6d2df084d24eba
Maizlow <johan@maizlow.se>
2023-07-26 11:11:36 +0200

    a

--- client/variables.py
+++ client/variables.py
@@ -6,4 +6,4 @@ rel_path1 = "../"
 CERT_FILEPATH = (cwd / rel_path1 / "IOT_Demo_Certs/device.pem.crt").resolve().__str__() #"/home/abece/certs/device.pem.crt"
 PRI_KEY_FILEPATH = (cwd / rel_path1 / "IOT_Demo_Certs/private.pem.key").resolve().__str__() #"/home/abece/certs/private.pem.key"
 CA_FILEPATH = (cwd / rel_path1 / "IOT_Demo_Certs/AmazonRootCA1.pem").resolve().__str__() #"/home/abece/certs/AmazonRootCA1.pem"
-ALARM_LOG_CSV_FILEPATH = (cwd / "share").resolve().__str__()
+ALARM_LOG_CSV_FILEPATH = (cwd / rel_path1 / rel_path1 / "share").resolve().__str__()
